e84b4d77e23804f4067a972d464da5ce5b7e6f2d
Troy Tamas <tvtamas@gmail.com>
2023-11-25 02:18:20 +0000

    added first script and notebook and reorganized

---
+++ first_script.py
@@ -0,0 +1,14 @@
+print("Hello everybody!")
+# print("こんにちは！")
+# "むし"
+# print("😍")
+
+# message = "むし"
+# print(message)
+
+# name = "Troy"
+# print(f"Hello, my name is {name}")
+
+# x = 5 + 4
+# print(x)
+# print(f"5 + 4 = {x}")
